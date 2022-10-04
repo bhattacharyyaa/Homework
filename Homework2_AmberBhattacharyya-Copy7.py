751a3b15de7a76aa0ddd24ff47f656490a73561f
bhattacharyyaa <112636408+bhattacharyyaa@users.noreply.github.com>
2022-10-03 23:17:50 -0400

    Add files via upload

---
+++ Homework2_AmberBhattacharyya-Copy7.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+n=0
+while n<10:
+    print(n)
+    if n==5:
+        break 
+    n+=1
+
+    
+
+
+# In[ ]:
+
+
+n=0
+while n<5:
+    print(n)
+    if n==5: 
+print(n "is not less than 5")
+    
+
+
+# In[ ]:
+
+
+fruits=["oranges", "Berries", "Peaches", "apples"]
+    for fruit in fruits:
+    print(type("I like" fruits))
+    if item==apple
+        print("apples aren't fruit")
+
+
+# In[ ]:
+
+
+numbers=list(range(30))
+sum(numbers)
+    print(sum)
+
+
+# In[ ]:
+
+
+grade= 60
+if grade >=90:
+    print("A")
+elif grade <=79:
+    print("C")
+else:
+    print("F") 
+
+
+# In[ ]:
+
+
+marks={'emma':98,'Brad':82,'Chris':71,"Mia":93}
+print(marks)
+print(mean(marks)
+print(min(marks))
+print(max(marks))
+
+
+# In[ ]:
+
+
+print(key(marks))
+if key==Chris
+    break 
+    
+
+
+# In[ ]:
+
+
+print(key(marks))
+if key==Chris
+True 
+
