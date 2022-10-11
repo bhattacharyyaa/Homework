c3e2b16292027b406d16309aca5211fde2156e98
bhattacharyyaa <112636408+bhattacharyyaa@users.noreply.github.com>
2022-10-11 02:03:54 -0400

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+name= 'mark''pete''mary
+grades={'mark':88,'pete':90,'mary':76}
+for name in grades():
+    if names == name:
+        print(grades)
+        break 
+else:
+    print('You cannot find this students name')
+
+
+# In[ ]:
+
+
+n=8
+while n<8:
+    print(n)
+else: print(8)
+    
+
+
+# In[ ]:
+
+
+p=0
+while p=0:
+    p=p+1
+    print(p)
+
+
+# In[ ]:
+
+
+x=1
+for x=1
+    x+1
+    print(x)
+
+
+# In[ ]:
+
+
+num=range(1,100)
+    print(mean(num))
+    print(sum(num))
+    print(stdev(num))
+
+
+# In[ ]:
+
+
+list={1,2,3,4}
+    print(len(list))
+    list.remove(1)
+    list.append(6)
+    print(list)
+    
+
+
+# In[ ]:
+
+
+str(1)= "are"
+str(2)="you"
+str(3)="okay"
+print(str(1)+str(2)+str(3))
+
